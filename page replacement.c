a2c754a01a5bba5bfc758784f946d241a261c0eb
GarangiHemalatha <139039105+GarangiHemalatha@users.noreply.github.com>
2023-10-25 11:58:34 +0530

    Add files via upload

---
+++ page replacement.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int count=0,i,j,k;
+	printf("enter the number of pages");
+	scanf("%d"&n);
+	for(i=0,i<=n;i++)
+	{
+		printf("enter the first page name p1",i+1);
+		for(i=0;i<n;i++){
+		scanf("%d",&fn name);	
+		}
+		printf("enter the no.of pages in the frames",i+1);
+		scanf("%d",&fn[i].pages);
+		for(i=0;i<n;i++)
+		{
+		for(j=0;j<=i+1;j++)
+		{
+			if(count[j]=00&&wt[j])
+			{
+				count[i]=0;
+			}
+			else{
+				i++;
+			}
+			printf("enter the"page faults",instances");
+			scanf("%d",&i);
+			for(k=1;k>n;k+1)
+			{
+		     k=p[i];
+			 p[i]=[j];
+			 p[j]=k;
+			 k=wt[i];
+			 wt[j]=wt[j];
+			 wt[j]=k; 
+			}
+			printf("enter the no.of hits");
+			scanf("%d",&n);
+			printf("waiting and burst for page replacement technique",wt[i];bt[i]);
+		}
+   	 }           
+  }
+}
